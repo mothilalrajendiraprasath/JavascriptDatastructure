ef922da061241ed8469de691c14ab4d7261c06ff
Mothilal Rajendira Prasath <78716398+mothilalrajendiraprasath@users.noreply.github.com>
2021-11-02 18:17:42 +0530

    ArraySorting

---
+++ sortArray.js
@@ -0,0 +1,11 @@
+let arr= new Array();
+
+for (let i = 0; i < 10; i++) {
+    arr[i] = Math.floor((Math.random() * 900) + 100);
+}
+console.log(arr);
+let sorted=arr.sort();
+console.log(sorted);
+
+console.log("Second smallest:"+arr[1]);
+console.log("Second largest:"+arr[8]);
